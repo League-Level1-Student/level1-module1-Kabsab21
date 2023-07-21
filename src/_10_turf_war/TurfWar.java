a3adb5df3b489a7844aeab66e605b3956726f26d
league <liangyuanli2020@gmail.com>
2023-07-21 16:58:53 -0700

    mybrain

--- src/_10_turf_war/TurfWar.java
+++ src/_10_turf_war/TurfWar.java
@@ -41,12 +41,13 @@ public Player(int x, int y, int speed, int playerSize, int playerColor, int upKe
         boolean moveRight = false;
         int pixelCount = 0;
 
-        void drawPlayer() {
+        void drawPlayer(){
             /*
              * 2. Draw a rectangle to represent the the Player using its color,
              * coordinates and size.
              */
-            
+        	fill(playerColor);
+           rect(x,y, playerSize, playerSize);
         }
 
         void update() {
@@ -61,7 +62,15 @@ public Player(int x, int y, int speed, int playerSize, int playerColor, int upKe
             if (moveUp && y > statsBoardLine) {
                 y-=speed;
             }
-            
+          if ( moveDown && y < 0) {
+        	  y+=speed;
+          }
+          if (moveRight && x > statsBoardLine) {
+              x-=speed;
+          }
+          if ( moveLeft && x < 0) {
+        	  x+=speed;
+          }
             /* 
              * 3. Make the Player move in every other direction.
              * 
@@ -118,9 +127,9 @@ public Player(int x, int y, int speed, int playerSize, int playerColor, int upKe
      * 4. Declare two variables of the Player class called player1 and player2.
      * Do not initialize them yet.
      */
-
+  Player player1;
+  Player player2;
     
-
     // Do not change these variables
     boolean gameOver = false;
     final int W = 87;
@@ -139,7 +148,7 @@ public Player(int x, int y, int speed, int playerSize, int playerColor, int upKe
     @Override
     public void settings() {
         // 5. Set the size for your sketch. Make it at least 300x300.
-       
+       setSize(300,300);
     }
 
     @Override
@@ -151,10 +160,10 @@ public Player(int x, int y, int speed, int playerSize, int playerColor, int upKe
         ((java.awt.Canvas) surface.getNative()).requestFocus();
 
         // 6. Set the background color.
-
+ fill(100,100,100);
 
         // 7. Call the noStroke Method.
-        
+        noStroke();
         
         /*
          * 8. Initialize the two Player objects. For one use UP, LEFT, DOWN,
@@ -168,85 +177,87 @@ public Player(int x, int y, int speed, int playerSize, int playerColor, int upKe
          * not select black, white or the color you used for your background as it
          * will give that player an unfair advantage.
          */
+       player1 = new Player(50, 70, 10, 10, 555555, W, A, S, D);
+       player2 = new Player(100, 70, 10, 10, 888867, UP, LEFT, DOWN, RIGHT);
+       }
 
-        
+
+   
+
+    public void isGameOver() {
+        if (millis() >= endOfGame && !gameOver) {
+            gameOver = true;
+        }
     }
 
-    /*
-     * 9. Uncomment these methods once you have created and initialized player 1
-     * and player 2
-     */
+    public void endGame() {
+        
+        String winMessage = "";
+        
+        double player1Percentage = calculateRoundedPixelPercentage(player1.pixelCount);
+        double player2Percentage = calculateRoundedPixelPercentage(player2.pixelCount);
+        
+        
+        if (player1Percentage == player2Percentage) {
+            winMessage = "TIE";
 
-//    public void isGameOver() {
-//        if (millis() >= endOfGame && !gameOver) {
-//            gameOver = true;
-//        }
-//    }
-//
-//    public void endGame() {
-//        
-//        String winMessage = "";
-//        
-//        double player1Percentage = calculateRoundedPixelPercentage(player1.pixelCount);
-//        double player2Percentage = calculateRoundedPixelPercentage(player2.pixelCount);
-//        
-//        
-//        if (player1Percentage == player2Percentage) {
-//            winMessage = "TIE";
-//
-//        } else if (player1Percentage > player2Percentage) {
-//            
-//            winMessage = "PLAYER 1 WINS!";
-//            
-//        } else {
-//            winMessage = "PLAYER 2 WINS!";
-//        }
-//        
-//        text(winMessage, (width/2 - (winMessage.length()*statsBoardSpacing) / 4)  , statsBoardSpacing*4);
-//        
-//        noLoop();
-//    }
-//    
-//    public void displayStats() {
-//        
-//        fill(Color.BLACK.getRGB());
-//        rect(0,0, width, 100);
-//        fill(Color.WHITE.getRGB());
-//        textSize(24);
-//        
-//        int gameTimeLeft = (endOfGame / 1000) - (int)(millis() / 1000);
-//        String timerDisplay = "Seconds Left: " + gameTimeLeft;
-//        text(timerDisplay, (width/2 - (timerDisplay.length()*statsBoardSpacing) / 4)  , statsBoardSpacing);
-//        
-//        loadPixels();
-//        player1.countPixels();
-//        player2.countPixels();
-//        
-//        String player1Display = "Player 1 Coverage: " + calculateRoundedPixelPercentage(player1.pixelCount) + "%";
-//        text(player1Display, (width/2 - (player1Display.length()*statsBoardSpacing) / 4)  , statsBoardSpacing*2);
-//        
-//        String player2Display = "Player 2 Coverage: " + calculateRoundedPixelPercentage(player2.pixelCount) + "%";
-//        text(player2Display, (width/2 - (player2Display.length()*statsBoardSpacing) / 4)  , statsBoardSpacing*3);
-//    }
-//    
-//    public double calculateRoundedPixelPercentage(int pixelCount) {
-//        double pixelPercentage = (pixelCount / (double) (width * height - width * statsBoardLine )) * 100;
-//        double pixelsRounded = Math.round(pixelPercentage * 100) / 100.0;
-//        return pixelsRounded;
-//    }
+        } else if (player1Percentage > player2Percentage) {
+            
+            winMessage = "PLAYER 1 WINS!";
+            
+        } else {
+            winMessage = "PLAYER 2 WINS!";
+        }
+        
+        text(winMessage, (width/2 - (winMessage.length()*statsBoardSpacing) / 4)  , statsBoardSpacing*4);
+        
+        noLoop();
+    }
+    
+    public void displayStats() {
+        
+        fill(Color.BLACK.getRGB());
+        rect(0,0, width, 100);
+        fill(Color.WHITE.getRGB());
+        textSize(24);
+        
+        int gameTimeLeft = (endOfGame / 1000) - (int)(millis() / 1000);
+        String timerDisplay = "Seconds Left: " + gameTimeLeft;
+        text(timerDisplay, (width/2 - (timerDisplay.length()*statsBoardSpacing) / 4)  , statsBoardSpacing);
+        
+        loadPixels();
+        player1.countPixels();
+        player2.countPixels();
+        
+        String player1Display = "Player 1 Coverage: " + calculateRoundedPixelPercentage(player1.pixelCount) + "%";
+        text(player1Display, (width/2 - (player1Display.length()*statsBoardSpacing) / 4)  , statsBoardSpacing*2);
+        
+        String player2Display = "Player 2 Coverage: " + calculateRoundedPixelPercentage(player2.pixelCount) + "%";
+        text(player2Display, (width/2 - (player2Display.length()*statsBoardSpacing) / 4)  , statsBoardSpacing*3);
+    }
+    
+    public double calculateRoundedPixelPercentage(int pixelCount) {
+        double pixelPercentage = (pixelCount / (double) (width * height - width * statsBoardLine )) * 100;
+       double pixelsRounded = Math.round(pixelPercentage * 100) / 100.0;
+       return pixelsRounded;
+   }
 
     @Override
     public void draw() {
         // 10. Call the drawPlayer method for both players.
-        
+    	 player1.drawPlayer();
+    	 player2.drawPlayer();
         // 11. Call the update method for both players.
-
+         player1.update();
+         player2.update();
         // 12. Call the isGameOver method.
-        
+        isGameOver();
         // 13. Call the displayStats method.
-
+        displayStats();
         // 14. If gameOver is true call the endGame method.
-
+        if( gameOver == true) {
+        	endGame();
+        	}
     }
 
     @Override
@@ -255,7 +266,8 @@ public Player(int x, int y, int speed, int playerSize, int playerColor, int upKe
          * 15. Call the enableMovement method for both players and pass keyCode
          * to the method.
          */        
-
+player1.enableMovement(keyCode);
+player2.enableMovement(keyCode);
     }
 
     @Override
@@ -264,7 +276,8 @@ public Player(int x, int y, int speed, int playerSize, int playerColor, int upKe
          * 16. Call the disableMovement method for both players and pass keyCode
          * to the method.
          */
-
+player1.disableMovement(keyCode);
+player2.disableMovement(keyCode);
     }
 
     /*
